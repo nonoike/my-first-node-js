537c6f0bc0f264addd1c8663fea9f5678460376d
nonoike <ij4nu8d4fw@gmail.com>
2017-11-04 23:35:36 +0900

    add comment

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-'use strict';
-const number = process.argv[2] || 0;
+'use strict'; // node.jsではモジュールがあるため、無名関数は不要
+/*
+process.argvの添字:
+  0: node のファイルのパス
+  1: 実行中プログラムのファイルのパス
+  2: 最初の引数
+*/
+const number = process.argv[2] || 0; // ||の左式(process.argv[2])がfalseなら右式(0)を返す
 let sum = 0;
 for (let i = 1; i <= number; i++) {
     sum = sum + i;
